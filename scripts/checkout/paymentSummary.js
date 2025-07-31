0733b41ea16862462470df3541be9305449595e5
yuumy7 <alisasergeevna777@gmail.com>
2025-07-31 13:43:32 +0500

    create order with backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/delivery-options.js';
 import { formatCurrency } from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
 
 export  function renderPaymentSummary () {
 let productPriceCents = 0;
@@ -65,4 +66,27 @@ let shippingPriceCents = 0;
 
  document.querySelector('.js-payment-summary')
  .innerHTML = paymentSummaryHTML;
+
+ document.querySelector('.js-place-order')
+ .addEventListener('click', async () => {
+  try{
+    const response = await fetch('https://supersimplebackend.dev/orders', {
+      method: 'POST', 
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        cart: cart
+      })
+     });
+    
+     const order = await response.json();
+     addOrder(order);
+
+  } catch(error) {
+  console.log('Unexpected error.Try again later.');
+  }
+
+  window.location.href = 'orders.html';
+ });
 }
